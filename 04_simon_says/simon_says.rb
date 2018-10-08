e7e5411169289b0ea48006c3c6e531f848a4c465
DamVan75 <damienvaneeckhout@gmail.com>
2018-10-08 19:52:24 +0200

    exo 4 sauf dernier test

--- 04_simon_says/simon_says.rb
+++ 04_simon_says/simon_says.rb
@@ -1,11 +1,43 @@
 def echo(x)
-	 x
+	x
 end
 
-def shout(x, *p, y)
-	x = x.upcase + y.upcase
+def shout(x)
+	x.upcase
 end
 
-def perform
-	shout("hello", "world")
+def repeat(x,*p)
+	if p == [] || p.join.to_i == 1 
+		then a = x + " " + x
+		return a
+	else
+		p = p.join.to_i	
+		a=""
+		p.times do 
+			a = a + x + " "
+		end
+		return a[0...-1]
+	end
 end
+
+def start_of_word(x,y)
+	b=x.length-y
+	x[0...-b]
+end
+
+def first_word(x)
+	b=x.length-x.index(" ")
+	x[0...-b]
+end
+
+def titleize(x)
+	words_to_ignore = ["the","and"]
+	if x.include?(' ')
+		then
+			new_x = x.split(' ')
+			new_x.each {|i| i[0].upcase if ! words_to_ignore.include? i }
+			new_x = new_x.join(' ')
+	end
+	x.capitalize
+end
+
