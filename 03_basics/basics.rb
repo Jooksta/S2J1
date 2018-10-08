a9baae09890a9a0ebb761c7618175d6c45b1bd83
casuis <rthrsmn@gmail.com>
2018-10-08 16:45:46 +0200

    modif exo_03

--- 03_basics/basics.rb
+++ 03_basics/basics.rb
@@ -1,6 +1,23 @@
 def who_is_bigger(a,b,c)
+	if (a == nil || b == nil || c == nil )
+		then return "nil detected"
+	end
+if (a > b && a > c)
+	return("a is bigger");
+elsif (b > a && b > c)
+	return ("b is bigger");
+else
+	return ("c is bigger")
+end
+end
 
-end 
+def reverse_upcase_noLTA(str)
+	return str.upcase.reverse.tr("LTA", "")
+end
+
+def array_42(array)
+	return array.join.scan(/42/).any?
+end
 
 def perform
     who_is_bigger(84,42,nil)
@@ -8,5 +25,9 @@ def perform
     who_is_bigger(84,42,21)
     who_is_bigger(42,84,21)
     who_is_bigger(42,21,84)
-
+	reverse_upcase_noLTA("Tries this at Home, Kids")
+	reverse_upcase_noLTA("Ponies loves carrots")
+	reverse_upcase_noLTA("qwertyuiopasdfghjkl;zxcvbn")
+	array_42([1, 2, 3, 4, 5, 6, 7 , 8, 9, 10])
+	array_42([1, 2, 3, 4, 5, 6, 7 , 8, 9, 42, 21, 10.5])
 end
