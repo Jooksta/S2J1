373ce2d402ce79ec87d969c7cacf2f9a339a101e
casuis <rthrsmn@gmail.com>
2018-10-08 17:51:15 +0200

    Fin exo_03

--- 03_basics/basics.rb
+++ 03_basics/basics.rb
@@ -19,6 +19,24 @@ def array_42(array)
 	return array.join.scan(/42/).any?
 end
 
+def magic_array(array)
+	array = array.flatten.sort
+	tab = []
+	i  = 0
+	array.each do |x|
+		unless ((x % 3) == 0 || tab.include?(x))
+			tab[i] = x;
+			i += 1;
+		end
+	end
+	i = 0
+	while (i < tab.length)
+		tab[i] = tab[i] * 2
+		i += 1
+	end
+	return tab
+end
+
 def perform
     who_is_bigger(84,42,nil)
     who_is_bigger(nil,42,21)
@@ -30,4 +48,7 @@ def perform
 	reverse_upcase_noLTA("qwertyuiopasdfghjkl;zxcvbn")
 	array_42([1, 2, 3, 4, 5, 6, 7 , 8, 9, 10])
 	array_42([1, 2, 3, 4, 5, 6, 7 , 8, 9, 42, 21, 10.5])
+	magic_array([1, 2, 3, 4, 5, 6])
+	magic_array([1, [2, 3], 4, 5, 6, 23, 31, [1, 2, 3]])
+	magic_array([[32, 54], [48, 12], [21, [1, 2, [3]]], 7, 8])
 end
