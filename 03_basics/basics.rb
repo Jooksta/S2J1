5d40877b2df0752c39f02ed205d4ae0a9a9ec6e7
DamVan75 <damienvaneeckhout@gmail.com>
2018-10-08 14:58:05 +0200

    début exo 3

--- 03_basics/basics.rb
+++ 03_basics/basics.rb
@@ -1,1 +1,12 @@
-# write your code here
+def who_is_bigger(a,b,c)
+
+end 
+
+def perform
+    who_is_bigger(84,42,nil)
+    who_is_bigger(nil,42,21)
+    who_is_bigger(84,42,21)
+    who_is_bigger(42,84,21)
+    who_is_bigger(42,21,84)
+
+end
